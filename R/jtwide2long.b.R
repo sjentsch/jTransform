53e861d294b9f23602562ea592a1d90e25a36152
Sebastian Jentschke <Sebastian.Jentschke@gmail.com>
2023-07-16 14:26:47 +0200

    Implemented transformation wide to long

--- R/jtwide2long.b.R
+++ R/jtwide2long.b.R
@@ -3,30 +3,36 @@ jtWide2LongClass <- if (requireNamespace('jmvcore', quietly=TRUE)) R6::R6Class(
     inherit = jtWide2LongBase,
     private = list(
         .run = function() {
-            # if not blnOut -> exit
-            if (!self$options$blnOut) {
-                print("jtwide2long - no blnOut")        
-                return()
+            # reset the output text, if not blnOut -> exit
+            self$results$txtOut$setContent("")
+            Sys.sleep(0.01)
+            if (!self$options$blnOut) return()
+
+            # add column attributes (measureType and dataTye)
+            crrDta <- jmvReadWrite:::jmvAtt(self$data)
+
+            # assemble and check output file name (chkFle in utils.R)
+            fleOut <- chkFle(self$options$fleOut)
+
+            # convert excLvl to a number
+            excLvl <- eval(parse(text = paste0("as.integer(c(", self$options$excLvl, "))")))
+            
+            # check whether all required variables are present and assemble and run jmvReadWrite command
+            if (length(self$options$varLst) < 1 | length(self$options$varID) < 1 | length(self$options$varTme) < 1) {
+                jmvcore::reject(.("“Variables that identify the same unit”, and “Variables to be transformed” are required and must not be empty."), code = "")
             }
 
-            print("jtwide2long")
-            print(self$options$blnOut)
-            print(self$options$fleOut)
-            print(self$options$chcEnI)
-            print(self$options$varEnI)
-            print(self$options$varID)
-            print(self$options$varOrd)
-            print(self$options$varSep)
-            print(self$options$varTme)
+            txtOut <- c()
+            jmvReadWrite::wide2long_omv(dtaInp = crrDta, fleOut = fleOut, varLst = self$options$varLst, varID = self$options$varID,
+                                        varTme = self$options$varTme, varExc = self$options$varExc, varSep = self$options$varSep, excLvl = excLvl)
 
-            # check whether there is a directory
-            # check existence of the output directory
-            # check existence of the output file
-            # assmeble and run jmvReadWrite command
-            
-            # `self$data` contains the data
-            # `self$options` contains the options
-            # `self$results` contains the results object (to populate)
+            while (!file.exists(fleOut)) Sys.sleep(0.01)
+            if (file.exists(fleOut)) {
+                txtOut <- c(txtOut, sprintf("<b>%s</b> successfully written to %s.", basename(fleOut), dirname(fleOut)))
+                self$results$txtOut$setContent(paste(txtOut, collapse = "<br>\n"))
+            } else {
+                jmvcore::reject(.("Error when writing '{file}' to '{dir}'."), code = '', file = basename(fleOut), dir = dirname(fleOut))
+            }
 
         })
 )
