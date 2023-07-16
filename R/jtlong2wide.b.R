7e74e1508aca4bdd02d33bce89f0b2bdda14485f
Sebastian Jentschke <Sebastian.Jentschke@gmail.com>
2023-07-16 14:27:11 +0200

    Implemented transformation long to wide

--- R/jtlong2wide.b.R
+++ R/jtlong2wide.b.R
@@ -3,30 +3,31 @@ jtLong2WideClass <- if (requireNamespace('jmvcore', quietly=TRUE)) R6::R6Class(
     inherit = jtLong2WideBase,
     private = list(
         .run = function() {
-            # if not blnOut -> exit
-            if (!self$options$blnOut) {
-                print("jtlong2wide - no blnOut")        
-                return()
+            # reset the output text, if not blnOut -> exit
+            self$results$txtOut$setContent("")
+            Sys.sleep(0.01)
+            if (!self$options$blnOut) return()
+
+            # add column attributes (measureType and dataTye)
+            crrDta <- jmvReadWrite:::jmvAtt(self$data)
+
+            # assemble and check output file name (chkFle in utils.R)
+            fleOut <- chkFle(self$options$fleOut)
+
+            txtOut <- c()
+
+            # assemble and run jmvReadWrite command
+# TO-DO: check whether all required variables are present
+            jmvReadWrite::long2wide_omv(dtaInp = crrDta, fleOut = fleOut, varID = self$options$varID, varTme = self$options$varTme,
+                                        varTgt = self$options$varTgt, varExc = self$options$varExc, varSep = self$options$varSep,
+                                        varOrd = self$options$varOrd, varAgg = self$options$varAgg)
+            while (!file.exists(fleOut)) Sys.sleep(0.01)
+            if (file.exists(fleOut)) {
+                txtOut <- c(txtOut, sprintf("<b>%s</b> successfully written to %s.", basename(fleOut), dirname(fleOut)))
+                self$results$txtOut$setContent(paste(txtOut, collapse = "<br>\n"))
+            } else {
+                jmvcore::reject(.("Error when writing '{file}' to '{dir}'."), code = '', file = basename(fleOut), dir = dirname(fleOut))
             }
-
-            print("jtlong2wide")
-            print(self$options$blnOut)
-            print(self$options$fleOut)
-            print(self$options$chcEnI)
-            print(self$options$varEnI)
-            print(self$options$varID)
-            print(self$options$varOrd)
-            print(self$options$varSep)
-            print(self$options$varTme)
-
-            # check whether there is a directory
-            # check existence of the output directory
-            # check existence of the output file
-            # assmeble and run jmvReadWrite command
-            
-            # `self$data` contains the data
-            # `self$options` contains the options
-            # `self$results` contains the results object (to populate)
 
         })
 )
