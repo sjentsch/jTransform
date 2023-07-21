59a9b8a5c85a8a5cacf505b09086710ce9959c8c
Sebastian Jentschke <Sebastian.Jentschke@gmail.com>
2023-07-21 20:52:43 +0200

    Re-organized jtArrangeCols to contain a preview and to open as a new session instead of writing the file to disc

--- R/jtarrangecols.b.R
+++ R/jtarrangecols.b.R
@@ -4,36 +4,32 @@ jtArrangeColsClass <- if (requireNamespace('jmvcore', quietly=TRUE)) R6::R6Class
     private = list(
 
         .run = function() {
+            self$results$txtOut$setContent(" ")
 
-            # reset the output text, if not blnOut -> exit
-            if (!self$options$blnOut) {
-                self$results$txtOut$setContent(" ")
-                return()
-            }
-            txtOut <- c()
+            # check whether all required variables are present
+            if (length(self$options$varOrd) > 0) {
 
-            # assemble (add directory - HOME on *nix, USERPROFILE on Windows -
-            # if necessary) and check existence of the directory, that the file
-            # doesn't exist already and the correct file extension
-            fleOut <- chkFle(self$options$fleOut)
-            
-            # add column attributes (measureType and dataTye)
-            crrDta <- jmvReadWrite:::jmvAtt(self$data)
+                # add column attributes (measureType and dataTye)
+                crrDta <- jmvReadWrite:::jmvAtt(self$data)
+                
+                # assemble and run jmvReadWrite command
+                dtaFrm <- jmvReadWrite::arrange_cols_omv(crrDta, varOrd = self$options$varOrd)
 
-            # check whether all required variables are present; assemble and run jmvReadWrite command if the output file doesn't already exist
-            if (!file.exists(fleOut) && length(self$options$varOrd) > 0) {
-                jmvReadWrite::arrange_cols_omv(crrDta, fleOut = fleOut, varOrd = self$options$varOrd)
-                if (file.exists(fleOut)) {
-                    txtOut <- c(txtOut, sprintf("<strong>%s</strong> successfully written to %s.", basename(fleOut), dirname(fleOut)))
-                    self$results$txtOut$setContent(paste(paste0("<p>", txtOut, "</p>"), collapse = "<p></p>\n"))
+                # depending on whether CREATE was pressed (blnOut == TRUE), either use the data
+                # for preview or open a new jamovi session with them
+                if (blnOut) {
+                    tmpOut <- paste0(tempfile(), ".omv")
+                    jmvReadWrite::write_omv(dtaFrm, fleOut = tmpOut)
+# TO-DO: replace Dataset with the name of the current data set
+                    system(paste0(jmvEXE(), " --temp --title=\"", paste0("Dataset", "_arrCol"), "\" ", tmpOut))
                 } else {
-                    self$results$txtOut$setContent(sprintf("<p><strong>Error</strong> when writing %s to %s.</p>", basename(fleOut), dirname(fleOut)))
+                    self$results$txtPvw$setContent(paste0("Variables in the output file:\n", strSpl(names(dtaFrm), maxLng = 80), print(data[1:10, 1:8], )))
                 }
-            } else if (file.exists(fleOut)) {
-                self$results$txtOut$setContent(
-                  sprintf("<p><strong>%s already exists</strong>. Change the name of the output file or remove the exisiting file.</p>", basename(fleOut)))
+
             } else {
+
                 self$results$txtOut$setContent("<p>“Desired order of variable(s)” is required and must not be empty.</p>")
+
             }
 
         })
