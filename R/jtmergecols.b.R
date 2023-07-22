908506eb20276d6792e765f8e71df73eaadbf9c4
Sebastian Jentschke <Sebastian.Jentschke@gmail.com>
2023-07-23 01:07:22 +0200

    jtMergeCols working

--- R/jtmergecols.b.R
+++ R/jtmergecols.b.R
@@ -1,20 +1,18 @@
-
-# This file is a generated template, your changes will not be overwritten
-
 jtMergeColsClass <- if (requireNamespace('jmvcore', quietly=TRUE)) R6::R6Class(
     "jtMergeColsClass",
     inherit = jtMergeColsBase,
+
     private = list(
 
         .run = function() {
 
             # check whether all required variables are present
-            if (length(self$options$varBy) > 0 && dim(self$data)[2] > 1) {
+            if (length(self$options$varBy) > 0 && dim(self$data)[2] > 1 && nzchar(self$options$fleInp)) {
 
                 # add column attributes (measureType and dataTye)
                 # and attach further input files as attribute fleInp
                 crrDta <- jmvReadWrite:::jmvAtt(self$data)
-                attr(crrDta, "fleInp") <- fndFlI(self$options$fleInp)
+                attr(crrDta, "fleInp") <- private$.fndFlI()
 
                 # assemble and run jmvReadWrite command
                 dtaFrm <- jmvReadWrite::merge_cols_omv(dtaInp = crrDta, varBy = self$options$varBy, typMrg = self$options$typMrg)
@@ -30,7 +28,28 @@ jtMergeColsClass <- if (requireNamespace('jmvcore', quietly=TRUE)) R6::R6Class(
                     system(paste0(jmvEXE(), " --temp --title=\"", paste0("Dataset", "_mergeCols"), "\" ", tmpOut))
                 }
 
+            } else {
+                self$results$txtPvw$setContent("")
             }
+            
+        },
 
-        })
+		.fndFlI = function() {
+			fleInp <- trimws(strsplit(self$options$fleInp, ";")[[1]])
+			for (i in seq_along(fleInp)) {
+				if (dirname(fleInp[i]) == ".") {
+				    tmpFlN <- list.files(path = hmeDir(), pattern = fleInp[i], recursive = TRUE);
+				    if (length(tmpFlN) == 1) {
+				        fleInp[i] <- file.path(hmeDir(), tmpFlN);
+				    } else {
+				        jmvcore::reject("'{file}' either doesn't exists or exists more than once in the home directory. Please add the exact path under “Input file(s)”.", file = fleInp[i])
+				    }
+				} else {
+				    fleInp[i] <- jmvReadWrite:::nrmFle(fleInp[i]);
+				}
+			}
+			return(fleInp)
+		}
+        
+    )
 )
