451187754988b98c05c1c6e31778a40c2beaf32d
Sebastian Jentschke <Sebastian.Jentschke@gmail.com>
2023-08-05 22:39:36 +0200

    Changed the custom UI element (reduced width to show properly on Windows)

--- jamovi/js/jtsearch.js
+++ jamovi/js/jtsearch.js
@@ -4,7 +4,7 @@ module.exports = {
 
     srcCst_creating(ui) {
         let $contents = ui.srcCst.$el;
-        $contents.append(`<input type="search" name="srcCst" class="text" size=50><br>`);
+        $contents.append(`<input type="search" name="srcCst" class="text" size=40><br>`);
         $contents.on("input", (f) => { if (this.crrPgr) { clearTimeout(this.crrPgr); }; this.crrPgr = setTimeout(() => { ui.srcTrm.setValue(`${f.target.value}`); this.crrPgr = null; }, 800); });
         ui.srcTrm.setValue("");
 //      ui.srcRes.setValue("");
