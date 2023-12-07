3eafb818166331b093f55b9f4c1dffb8f7213719
Sebastian Jentschke <Sebastian.Jentschke@gmail.com>
2023-12-07 17:23:41 +0100

    Added debugging functionality on Windows (output to file in the TEMP-directory)

--- R/debug.R
+++ R/debug.R
@@ -1,27 +1,40 @@
 j_DEBUG <- T
 j_INFO  <- T
 t_INFO  <- F
+fleWOS  <- ""
+# NB for Windows users: Feel free to change the path and name of the log file as you like.
+if (.Platform$OS.type == "windows") fleWOS <- file.path(base::Sys.getenv("TEMP"), "jTransform.log")
 
 #### Helper functions for debugging
 
 tinfo <- function(...) {
     if (!t_INFO) return(invisible(NULL))
 
+    if (nzchar(fleWOS)) base::sink(file = fleWOS, append = TRUE)
+
     cat(paste(list(...)))
     cat("\n")
+
+    if (nzchar(fleWOS)) base::sink()
 }
 
 jinfo <- function(...) {
     if (!j_INFO) return(invisible(NULL))
 
+    if (nzchar(fleWOS)) base::sink(file = fleWOS, append = TRUE)
+
     cat("\n")
     cat(paste(list(...)))
     cat("\n")
+
+    if (nzchar(fleWOS)) base::sink()
 }
 
 mark <- function(...) {
     if (!j_DEBUG) return(invisible(NULL))
   
+    if (nzchar(fleWOS)) base::sink(file = fleWOS, append = TRUE)
+
     if (missing(...)) {
         cat("Mark here\n")
         return(invisible(NULL))
@@ -32,4 +45,6 @@ mark <- function(...) {
     for (a in items)
         if (is.character(a)) cat(a, "\n") else print(a)
     cat("_____end_______\n\n")
+
+    if (nzchar(fleWOS)) base::sink()
 }
