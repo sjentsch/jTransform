a447f4a69e9cd1ae823699210b08449ee4aeb28a
Sebastian Jentschke <37706914+sjentsch@users.noreply.github.com>
2023-12-10 17:25:00 +0100

    Ensure that module doesn*t crash on Windows (jtmergecols.js)
    Adding a file chooser (``<input type="file"...``) doesn't work on Windows

--- jamovi/js/jtmergecols.js
+++ jamovi/js/jtmergecols.js
@@ -3,10 +3,15 @@
 module.exports = {
 
     fleChs_creating: function(ui) {
-        let $contents = ui.fleChs.$el;
-        $contents.append(`<label><input type="file" multiple accept=".omv,.csv,.sav,.xpt,.sas7bdat,.dta,.jasp" style="display: none;" /><span style="font-size: 1.2em;">Browse...</span></label>`);
-        $contents.on("change", (f) =>  { var crrTxt = ""; for (const crrFle of f.target.files) { crrTxt += `${crrFle.path}; `; }; ui.fleInp.setValue(crrTxt.slice(0, -2)); });
-        ui.fleInp.setValue("");
+        console.log("fleChs_creating")
+        if (navigator.userAgent.toLowerCase().indexOf(' electron/') > -1 && navigator.platform === 'Win32') {
+            console.log("Windows")
+        } else {
+            console.log("Linux, Mac or Cloud")
+            let $contents = ui.fleChs.$el;
+            $contents.append(`<label><input type="file" multiple accept=".omv,.csv,.sav,.xpt,.sas7bdat,.dta,.jasp" style="display: none;" /><span style="font-size: 1.2em;">Browse...</span></label>`);
+            $contents.on("change", (f) => { var crrTxt = ""; for (const crrFle of f.target.files) { crrTxt += `${crrFle.path}; `; }; ui.fleInp.setValue(crrTxt.slice(0, -2)); });
+        }
     }
 
 };
